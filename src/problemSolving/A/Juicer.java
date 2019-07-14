520a4a7320c1764be80e7cb6692b3a6d7172b622
Abdel Raouf <abdelraouf.ali@cashcall.com.eg>
2019-07-14 09:31:44 +0200

    solving juicer porblem in 14/7/2019,Reminder: we need to add some problems we solved before to the repo

---
+++ src/problemSolving/A/Juicer.java
@@ -0,0 +1,30 @@
+package problemSolving.A;
+
+import java.util.Scanner;
+
+public class Juicer {
+
+    public static void main(String Args[]){
+        Scanner scanner =  new Scanner(System.in);
+        int no_of_orange = scanner.nextInt();
+        int size_orange_not_exceed = scanner.nextInt();
+        int waste_section_capacity = scanner.nextInt();
+        int orange_be_squeezed_in_order;
+        int sum_of_orange_in_waste_section = 0;
+        int no_of_waste_section_to_be_emptied = 0;
+
+        for(int i = 0; i < no_of_orange; ++i){
+            orange_be_squeezed_in_order = scanner.nextInt();
+            if(orange_be_squeezed_in_order > size_orange_not_exceed){
+                continue; //continue -> jumps immediately to the next iteration of the loop without executing the instructions below it in the loop body
+            }else {
+                sum_of_orange_in_waste_section += orange_be_squeezed_in_order;
+                if(sum_of_orange_in_waste_section > waste_section_capacity){
+                    no_of_waste_section_to_be_emptied++;
+                    sum_of_orange_in_waste_section = 0;
+                }
+            }
+        }
+        System.out.print(no_of_waste_section_to_be_emptied);
+    }
+}
