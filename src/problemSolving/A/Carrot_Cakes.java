92f12c966be5f1c88a4ebb2332659106c3569f53
Abdel Raouf <abdelraouf.ali@cashcall.com.eg>
2019-07-21 07:15:46 +0200

    carrot cakes problem solved, at 21/7/2019

--- src/problemSolving/A/Carrot_Cakes.java
+++ src/problemSolving/A/Carrot_Cakes.java
@@ -3,7 +3,7 @@ package problemSolving.A;
 import java.util.Scanner;
 
 public class Carrot_Cakes {
-// not solved yet
+
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         int no_of_cakes_needed = scanner.nextInt();
@@ -11,17 +11,26 @@ public class Carrot_Cakes {
         int no_of_cakes_backed_simul = scanner.nextInt();
         int time_to_build_sec_oven = scanner.nextInt();
 
-        int oven_one = (no_of_cakes_needed/no_of_cakes_backed_simul) * time_needed_to_bake_k_cakes;
-        int oven_two = time_needed_to_bake_k_cakes + time_to_build_sec_oven;
-        double time_one_oven_taken =  Math.ceil(no_of_cakes_needed / no_of_cakes_backed_simul) * time_needed_to_bake_k_cakes;
-        if(Math.max(oven_one, oven_two) < time_one_oven_taken){
+        int no_of_times_oven_used = (no_of_cakes_needed + no_of_cakes_backed_simul - 1) / no_of_cakes_backed_simul; //Ceil implemented manually
+
+        int oven_1 = 0, oven_2 = time_to_build_sec_oven;
+
+        for(int i = 0; i < no_of_times_oven_used; i++){
+            if(oven_1 <= oven_2){
+                oven_1 += time_needed_to_bake_k_cakes;
+            }else {
+                oven_2 += time_needed_to_bake_k_cakes;
+            }
+        }
+
+        int time_needed_to_use_one_oven = no_of_times_oven_used * time_needed_to_bake_k_cakes;
+// if time needed to use one oven bigger than the max of the two ovens then we need another oven
+        if(Math.max(oven_1,oven_2) < time_needed_to_use_one_oven){
             System.out.println("YES");
-        }else{
+        }else {
             System.out.println("NO");
         }
+
+
     }
 }
-
-
-// using ceil is wrong from the beginning cuz if we use this example (599 97 54 992) if we make math.ceil(599/54) it will give us 11 and if we make 11 * 54 = 594 which is less than no of cakes needed (599)
-//  if we use n+k = 599 + 54 = 653 which is bigger than 599 so it is an accepted value so we need to use ceil+1 or n+k/k
